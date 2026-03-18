70297c8b52997c1495a3bc2b7d3ee0d317d60b4c
SuperKay <adegbesanoluwakayode52@gmail.com>
2026-03-18 17:34:00 +0100

    update

--- app/dashboard/customer/payment/success/page.tsx
+++ app/dashboard/customer/payment/success/page.tsx
@@ -1,7 +1,7 @@
 // app/dashboard/customer/payment/success/page.tsx
 'use client'
 
-export const dynamic = 'force-dynamic'
+export const revalidate = 0 
 
 import { useEffect, useState } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
